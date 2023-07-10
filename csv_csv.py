478f4352bdfc9d1795d7ba2c8316e1844d08f395
Maxim Danilov <139152367+danilov-maxim@users.noreply.github.com>
2023-07-10 20:29:25 +0200

    Update csv_csv.py

--- csv_csv.py
+++ csv_csv.py
@@ -1,3 +1,18 @@
+# Swap csv columns
+# Stdin is a small csv. Swap the columns in such a way that "1,2,3\nMike,Dyke,Pike" becomes "1,3,2\nMike,Pike,Dyke" and write to stdout.
+
+# sample Input
+# 1,2,3,4
+# a,b,c,d
+# Sample Output
+# 1,3,2,4
+# a,c,b,d
+
+# Sample Input
+# 1,"2,3",4
+# Sample Output
+# 1,4,"2,3"
+
 import csv
 import sys
 
@@ -10,17 +25,5 @@ writer = csv.writer(sys.stdout)
 for line in data:
     if len(line) > 2:
         line = [line[0], line[2], line[1], *line[3:]]
-    print(','.join(str(bit) for bit in line))
-
-import csv
-import sys
-
-
-read_file = sys.stdin.readlines()
-data = list(csv.reader(read_file, delimiter=','))
-
-writer = csv.writer(sys.stdout)
-for line in data:
-    if len(line) > 2:
-        line = [line[0], line[2], line[1], *line[3:]]
+    # print(','.join(str(bit) for bit in line))
     writer.writerow(line)
